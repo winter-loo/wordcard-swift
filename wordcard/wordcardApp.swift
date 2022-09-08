7bac4e402d0522265aebec0ee613bb06353913af
adjkldd <adjkldd@126.com>
2022-09-08 14:24:36 +0800

    Initial Commit

---
+++ wordcard/wordcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordcardApp.swift
+//  wordcard
+//
+//  Created by ldd on 2022/9/8.
+//
+
+import SwiftUI
+
+@main
+struct wordcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
